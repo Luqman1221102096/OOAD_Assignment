09e7068816a5925438a0879215b6d9cef5b8c3e0
Luqman1221102096 <1221102096@student.mmu.edu.my>
2025-01-19 16:31:52 +0800

    Delete Controller.java

--- Controller.java
+++
@@ -1,9 +0,0 @@
-// Controller class facilitates communication between the View and Model class
-public class Controller {
-    View view;
-    Model model;
-    public Controller(View view, Model model){
-        this.view = view;
-        this.model = model;
-    }    
-}
